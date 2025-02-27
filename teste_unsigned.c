f0f6d3fa2cd86b64fb816a011cb18fbed41091f1
edigardkayky <edigarzinhoribeiro.kayky@gmail.com>
2025-02-27 13:22:12 +0000

    tipos de modificadores de variaveis

---
+++ teste_unsigned.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+    unsigned long int numero_alto_sem_negativo = 300000000;
+    int numero_medio_com_negativo = 3000000000;
+
+    printf("seu numero medio com negativo é : %d\n",numero_medio_com_negativo);
+    printf("seu numero alto sem negativo é: %u\n",numero_alto_sem_negativo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
