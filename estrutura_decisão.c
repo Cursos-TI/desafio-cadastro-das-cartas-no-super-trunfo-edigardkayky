727adb46f04f21624337750bd9d4ed118006ce8d
edigardkayky <edigarzinhoribeiro.kayky@gmail.com>
2025-03-26 13:12:50 +0000

    estruturas de decisão

---
+++ estrutura_decisão.c
@@ -0,0 +1,12 @@
+int main() {
+    int numero = 12;
+    int numero2 = 25;
+  
+    if (numero <= numero2) {
+      printf("O número é igual\n");}
+     else {
+      printf("O número é diferente\n");
+    }
+  
+    return 0;
+  }
--- ola mundo.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){ printf ("tudo indo certo por aquiiiiii\n");
-return 0;
-
-}
